0d94fc445b1283c934c507369924f0fa31a21c1b
Hans <jy29825377@gmail.com>
2018-12-01 17:46:53 +0800

    Fix bug for Windows

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import path from 'path'
 import fs from 'fs'
 import main from './main'
 
+const user_home = process.env.HOME || process.env.HOMEPATH
+
 const argv = process.argv.slice(2)
-const config_file = path.join(process.env.HOME, '.init', 'config.json')
+const config_file = path.join(user_home, '.init', 'config.json')
 let config = {}
 
 if (!fs.existsSync(config_file)) {
-	fs.mkdirSync(path.join(process.env.HOME, '.init'))
+	fs.mkdirSync(path.join(user_home, '.init'))
 	fs.writeFileSync(config_file, JSON.stringify(config), 'utf8')
 } else {
 	config = require(config_file)
